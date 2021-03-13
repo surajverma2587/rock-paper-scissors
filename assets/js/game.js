0d01ae225433accf05fb27d70896b5f21eb88b7e
Suraj Verma <surajverma@rentalcars.com>
2021-03-13 13:14:37 +0000

    inital js plan comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-console.log("It works!!");
+// Declare global data
+
+// Prompt player for choice
+
+// validate player choice
+
+// randomly generate computer choice
+
+// apply game logic to check who wins
+
+// update scores of winner/loser
+
+// display scores
+
+// present confirm option to play again
